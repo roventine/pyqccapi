6491e3ceffbddf00f87633df6bf4a3a5afd2aef7
Apricity <sophiejiang1029@gmail.com>
2021-01-24 13:08:23 +0800

    release edition 0.1.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pyqccapi",
-    version="0.1.2",
+    version="0.1.3",
     author="roventine",
     author_email="ukyotachibana@yeah.net",
     description="simple api invoker for qcc",
