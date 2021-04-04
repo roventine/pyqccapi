c85c7a76809d5a4834175614249b3f954f096d3b
roventine <ukyotachibana@yeah.net>
2021-04-04 12:22:20 +0800

    add abs config path

--- pyqccapi/scenario/base_scenario.py
+++ pyqccapi/scenario/base_scenario.py
@@ -1,8 +1,12 @@
 from pyqccapi.task.api_task import Task
 from pyqccapi.task.task_invoker import ApiInvoker
 from pyqccapi.environment import *
+import os
 
-Environment('../config.yaml')
+path_config_dir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+path_config_file = os.path.join(path_config_dir,'config.yaml')
+
+Environment(path_config_file)
 
 
 def to_task_invoker(task: Task):
